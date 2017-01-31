59d0c678775c4bb758d3044445a42902d525ad01
Edward Yu <3dward.yu@gmail.com>
2017-01-31 01:24:45 -0500

    animate frontpage with typed.js

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".name").typed({
+        strings: ["I'm a photographer.", "I'm a web developer.", "I'm an adrenaline junkie.", "I'm Edward ^250 Yu ^400 :)" ],
+        typeSpeed: 20,
+        callback: function(ev) {
+          $(".description").fadeIn();
+        }
+
+
+});
+});
