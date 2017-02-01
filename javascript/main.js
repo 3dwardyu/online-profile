801f162539dfef5ecaef97cc4d5c8845914324a0
Edward Yu <3dward.yu@gmail.com>
2017-02-01 13:08:30 -0500

    styled home section

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".name").typed({
-        strings: ["I'm a photographer.", "I'm a web developer.", "I'm an adrenaline junkie.", "I'm Edward ^250 Yu ^400 :)" ],
+        strings: ["I'm a photographer.", "I'm a web developer.", "I'm an adrenaline junkie.", "I'm Edward ^250 Yu ^400 :)"],
         typeSpeed: 20,
         callback: function(ev) {
           $(".description").fadeIn();
